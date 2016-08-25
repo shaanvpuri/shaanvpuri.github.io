b797022df83d223724c69c11b0b3c86351448016
shaanvpuri <shaan@dhcp170.monkeyinferno.com>
2016-08-24 21:50:58 -0700

    Going to Dev

--- todobutton.js
+++ todobutton.js
@@ -6,15 +6,43 @@
 //- avoid global variables
 // - strive to make functions reusable
 
+function updateItemStatus() {
+	var cbID = this.id.replace("cb_", "");
+	var itemText = document.getElementById("item_" + cbID);
+
+	if (this.checked) {
+		itemText.className = "checked";
+
+	} else {
+		itemText.className = "";
+	}
+	
+
+
+}
 
 
 function addNewItem(list, itemText) {
-	var listItem = document.createElement("li");
-	listItem.innerText = itemText;
+	totalItems++;
 
+	var listItem = document.createElement("li");
+	var checkBox = document.createElement("input");
+	checkBox.type = "checkbox";
+	checkBox.id = "cb_" + totalItems;
+	checkBox.onclick = updateItemStatus;
+
+	var span = document.createElement("span");
+	span.innerText= itemText;
+	span.id = "item_" + totalItems;
+	listItem.appendChild(checkBox);
+	listItem.appendChild(span);
 	list.appendChild(listItem);
 
+
+
 }
+
+var totalItems = 0;
 
 var btnNew = document.getElementById("btnAdd");
 btnNew.onclick = function() {
@@ -28,7 +56,9 @@ btnNew.onclick = function() {
 	}
 	
 	addNewItem(document.getElementById("todoList"), itemText);
- 
+ 	
+ 	inItemText.focus();	
+ 	inItemText.select();
 
 	};
 
